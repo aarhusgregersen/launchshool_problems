1b4ba50c02c4ca7050f951659a49e15cf310a8e3
Martin Gregersen JEF <mag@jef.dk>
2016-07-05 17:09:38 +0200

    Finishes more easy4 tasks

---
+++ easy4/number_conversion.rb
@@ -0,0 +1,10 @@
+# Easy 4 - 9 Number conversion - Number to string
+
+def integer_to_string
+	
+end
+
+
+
+
+# Easy 4 - 10 Number conversion - Signed number to string
--- easy4/string_conversion.rb
+++ easy4/string_conversion.rb
@@ -1,6 +1,36 @@
-# Easy 4 - 7 String Conversion
+# Easy 4 - 7 String Conversion (to number)
+
+# Use hash to perform conversion
+DIGITS = {
+'0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4, 
+'5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9
+}
 
 def string_to_int(string)
-	
-	
+	digits = string.chars.map { |char| DIGITS[char] }
+
+	value = 0
+	digits.each { |digit| value = 10 * value + digit }
+	value
 end
+
+puts "7 - String conversion:"
+puts string_to_int('570')
+puts string_to_int('12372')
+
+# Easy 4 - 8 String conversion (to signed number)
+def string_to_signed_int(string)
+	case string[0]
+	when '-' then -string_to_int(string[1..-1])
+	when '+' then string_to_int(string[1..-1])
+	else	string_to_int(string)
+	end
+end
+
+puts "8 - String to signed number:"
+puts string_to_signed_int('123')
+puts string_to_signed_int('12372')
+puts string_to_signed_int('4321')
+puts string_to_signed_int('-570')
+puts string_to_signed_int('+100')
+
