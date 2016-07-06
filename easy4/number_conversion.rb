05fe4a45d72d47f8032a1494d92cfbc5d7a4c891
Martin Gregersen JEF <mag@jef.dk>
2016-07-06 10:37:28 +0200

    Completes next excercise

--- easy4/number_conversion.rb
+++ easy4/number_conversion.rb
@@ -1,10 +1,21 @@
 # Easy 4 - 9 Number conversion - Number to string
 
-def integer_to_string
-	
+DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+def integer_to_string(int)
+	string = ''
+
+	loop do
+		int, remainder = int.divmod(10)
+		string.prepend(DIGITS[remainder])
+		break if int == 0
+	end
+	string
 end
 
-
+puts integer_to_string(4321) 
+puts integer_to_string(0) 
+puts integer_to_string(5000) 
 
 
 # Easy 4 - 10 Number conversion - Signed number to string
